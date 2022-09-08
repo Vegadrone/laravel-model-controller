bd49e28cad066e62fad08e957dc858c5db0ae6be
Vegadrone <spenaantonino@gmail.com>
2022-09-08 16:10:31 +0200

    resolve __invoke error

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        $movies = Movie::all();
+        @dump($movies);
+        return view ('home', compact('movies'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('home');
 });
 
-Route::get('/', 'MovieController');
-
+Route::get('/Controllers', 'MovieController@index');
